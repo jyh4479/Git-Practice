1a4aed261be897e63b2819398ea7570496609fb5
Jeong yong hoon <jyh.dev@gmail.com>
2022-06-03 22:14:44 +0900

    Test Commit

---
+++ src/main/java/com/example/gitpractice/controllers/FirstController.java
@@ -0,0 +1,13 @@
+package com.example.gitpractice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/Test")
+    public String TestController() {
+        return "Hello";
+    }
+}
---
+++ src/main/java/com/example/gitpractice/services/FirstService.java
@@ -0,0 +1,4 @@
+package com.example.gitpractice.services;
+
+public class FirstService {
+}
